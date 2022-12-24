aa5ed8be2dc045956261766c142c641ff36e574d
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-12-24 13:50:34 -0500

    Added a dial..

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,14 @@ export default function Home() {
       <Head>
         <title>New Year Countdown</title>
       </Head>
-      
-      <main className={styles.stuffInMiddle}>
-        <h1 className={styles.time}>0 W, 0 D, 00:00:00</h1>
-        <p>Until 2023!!!</p>
+
+      <main className={styles.main}>
+        <div className={styles.dial}>
+          <div className={styles.stuffInMiddle}>
+            <h1 className={styles.time}>0 W, 0 D, 00:00:00</h1>
+            <p>Until 2023!!!</p>
+          </div>
+        </div>
       </main>
     </>
   )
