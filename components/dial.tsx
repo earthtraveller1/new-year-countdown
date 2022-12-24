78e6882d4b00e3b183aaa475fe4fa1a9b27846a1
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-12-24 14:06:53 -0500

    Formatted the project with new indentation.

--- components/dial.tsx
+++ components/dial.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import styles from "./dial.module.scss"
 
-export default function Dial({ children }: {children: React.ReactNode }) {
+export default function Dial({ children }: { children: React.ReactNode }) {
     return (
         <div className={styles.dial}>
             {children}
