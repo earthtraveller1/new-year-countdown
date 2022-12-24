d0c3a856b60a54160fe0224e993506bdc110c689
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-12-24 16:02:56 -0500

    Added a border.

--- components/dial.tsx
+++ components/dial.tsx
@@ -4,8 +4,12 @@ import styles from "./dial.module.scss"
 
 export default function Dial({ children }: { children: React.ReactNode }) {
     return (
-        <div className={styles.dial}>
-            {children}
+        <div className={styles.border} style={{
+            background: "conic-gradient(rgb(0, 0, 128) 0deg, black 64deg)"
+        }}>
+            <div className={styles.dial}>
+                {children}
+            </div>
         </div>
     )
 }
