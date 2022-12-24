a62416058dbd69fbf60fc3711262d0af5eb9477b
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-12-24 13:59:41 -0500

    Made the dial a separate component.

---
+++ components/dial.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./dial.module.scss"
+
+export default function Dial({ children }: {children: React.ReactNode }) {
+    return (
+        <div className={styles.dial}>
+            {children}
+        </div>
+    )
+}
