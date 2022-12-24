65fc83daad97894423c6c8b8b8e52fbc9d31e102
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-12-24 16:14:59 -0500

    Made the progress bar depend o n the time.

--- components/dial.tsx
+++ components/dial.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { TimeRange } from "../lib/utils";
 
 import styles from "./dial.module.scss"
 
 export default function Dial({ children }: { children: React.ReactNode }) {
+    let [progress, setProgress] = React.useState(0)
+    
+    React.useEffect(() => {
+        const timer = setInterval(() => {
+            const rightNow = new Date()
+            const newYearDay = new Date(rightNow.getFullYear() + 1, 0, 1)
+            
+            const range = new TimeRange(rightNow.getTime(), newYearDay.getTime())
+            
+            setProgress(range.fractionOfYearOver() * 360)
+        }, 1000)
+        
+        return () => { clearInterval(timer) }
+    }, [])
+    
     return (
         <div className={styles.border} style={{
-            background: "conic-gradient(rgb(0, 0, 128) 0deg, black 64deg)"
+            background: `conic-gradient(rgb(0, 0, 128) ${progress}deg, black 0deg)`
         }}>
             <div className={styles.dial}>
                 {children}
