5a14debd42eadf4d1318366adb0d816a2fec2c1f
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-12-24 14:09:45 -0500

    Made the countdown it's own component.

---
+++ components/countdown.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import styles from "./countdown.module.scss"
+
+export default function Countdown() {
+    return (
+        <div className={styles.countdown}>
+            <h1 className={styles.time}>0 W, 0 D, 00:00:00</h1>
+            <p>Until 2023!</p>
+        </div>
+    )
+}
