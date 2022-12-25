de65c6c74a5926e412804b5d1628fabf77446f70
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-12-25 11:01:52 -0500

    Padde the digits for the time numbers.

--- components/countdown.tsx
+++ components/countdown.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { TimeRange } from "../lib/utils";
+import { TimeRange, padNumber } from "../lib/utils";
 
 import styles from "./countdown.module.scss"
 
 export default function Countdown() {
     let [nextYear, setNextYear] = React.useState(new Date().getFullYear() + 1)
     
-    let [weeks, setWeeks] = React.useState(1)
-    let [days, setDays] = React.useState(0)
-    let [hours, setHours] = React.useState(0)
-    let [minutes, setMinutes] = React.useState(0)
-    let [seconds, setSeconds] = React.useState(0)
+    let [weeks, setWeeks] = React.useState('1')
+    let [days, setDays] = React.useState('0')
+    let [hours, setHours] = React.useState('0')
+    let [minutes, setMinutes] = React.useState('0')
+    let [seconds, setSeconds] = React.useState('0')
     
     React.useEffect(() => {
         const func = () => {
@@ -25,11 +25,11 @@ export default function Countdown() {
             const newYearDay = new Date(nextYear, 0, 1)
             const range = new TimeRange(rightNow.getTime(), newYearDay.getTime())
             
-            setWeeks(range.weeks)
-            setDays(range.days)
-            setHours(range.hours)
-            setMinutes(range.minutes)
-            setSeconds(range.seconds)
+            setWeeks(range.weeks.toString())
+            setDays(range.days.toString())
+            setHours(padNumber(range.hours, 2))
+            setMinutes(padNumber(range.minutes, 2))
+            setSeconds(padNumber(range.seconds, 2))
         }
         
         func()
