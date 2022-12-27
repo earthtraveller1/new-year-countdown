90a4a9732c549f5e02136fa9dc57eb120cbb92d5
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-12-27 14:24:35 -0500

    Animated the progress bar.

--- components/dial.tsx
+++ components/dial.tsx
@@ -7,19 +7,32 @@ export default function Dial({ children }: { children: React.ReactNode }) {
     let [progress, setProgress] = React.useState(0)
     
     React.useEffect(() => {
-        const func = () => {
+        const rightNow = new Date()
+        const newYearDay = new Date(rightNow.getFullYear() + 1, 0, 1)
+        
+        const range = new TimeRange(rightNow.getTime(), newYearDay.getTime())
+        const fullProgress = range.fractionOfYearOver() * 360
+        let innerProgress = 0
+        
+        function incrementProgress() {
+            if (innerProgress < fullProgress) {
+                setProgress(innerProgress)
+                innerProgress++
+                setTimeout(incrementProgress, 0.1)
+            }
+        }
+        
+        incrementProgress()
+    }, [])
+    
+    React.useEffect(() => {
+        const timer = setInterval(() => {
             const rightNow = new Date()
             const newYearDay = new Date(rightNow.getFullYear() + 1, 0, 1)
             
             const range = new TimeRange(rightNow.getTime(), newYearDay.getTime())
             
             setProgress(range.fractionOfYearOver() * 360)
-        }
-        
-        func()
-        
-        const timer = setInterval(() => {
-            func()
         }, 100000)
         
         return () => { clearInterval(timer) }
